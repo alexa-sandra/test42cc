443e8ff4bfc066e0ce8dde0c3227372daa03512f
alexa-sandra <alexa.sandra87@gmail.com>
2013-06-19 07:54:40 -0700

    ticket:5 fix disign

--- test42cc/person/forms.py
+++ test42cc/person/forms.py
@@ -3,7 +3,9 @@ from django.forms import ModelForm, Textarea
 from django.utils.safestring import mark_safe
 from models import Person
 from django import forms
-
+from django.utils.html import escape, conditional_escape
+from django.utils.encoding import force_unicode
+from django.forms.widgets import ClearableFileInput, Input, CheckboxInput
 
 class DatePickerWidget(forms.DateInput):
     class Media:
@@ -26,6 +28,21 @@ class DatePickerWidget(forms.DateInput):
             </script>'''%(name, self.params,))
 
 
+class CustomClearableFileInput(ClearableFileInput):
+
+    def render(self, name, value, attrs=None):
+        substitutions = {
+            'initial_text': self.initial_text,
+            'input_text': self.input_text,
+            'clear_template': '',
+            'clear_checkbox_label': '',
+            }
+        template = '%(input)s'
+        substitutions['input'] = Input.render(self, name, value, attrs)
+
+        return mark_safe(template % substitutions)
+
+
 class PersonForm(ModelForm):
     class Meta:
         model = Person
@@ -33,6 +50,7 @@ class PersonForm(ModelForm):
             'bio' : Textarea({'cols':40, 'rows':10}),
             'other_contacts' : Textarea({'cols':40, 'rows':10}),
             'birth_date':DatePickerWidget(params="dateFormat: 'dd.mm.yy', changeYear: true"),
+            'photo' :CustomClearableFileInput(),
                    }
 
 
