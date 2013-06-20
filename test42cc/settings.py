436a9d06af815b22402f3d4eba7b3e9b9799be50
alexa-sandra <alexa.sandra87@gmail.com>
2013-06-20 04:54:35 -0700

    ticket:8 fixing admin

--- test42cc/settings.py
+++ test42cc/settings.py
@@ -88,7 +88,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '(1zf^^qg&k6b7i9s5#vug9d8aw4)+ztt#&0adk5)9!vfv@)o^y'
