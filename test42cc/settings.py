7b127e7256274b4b9d9b5ddca89328ce089ee690
alexa-sandra <alexa.sandra87@gmail.com>
2013-06-20 00:15:24 -0700

    ticket:8 admin styles

--- test42cc/settings.py
+++ test42cc/settings.py
@@ -135,7 +135,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(PROJECT_ROOT, 'templates'),
-    os.path.join(PROJECT_ROOT, 'env/lib/python2.6/site-packages/easymode/templates'),
+    #os.path.join(PROJECT_ROOT, 'env/lib/python2.6/site-packages/easymode/templates'),
 )
 
 INSTALLED_APPS = (
--- test42cc/urls.py
+++ test42cc/urls.py
@@ -1,3 +1,4 @@
+import os
 from django.conf.urls import patterns, include, url
 
 from django.conf.urls.static import static
@@ -17,3 +18,12 @@ urlpatterns = patterns('',
      url(r'^requests/', views.storedRequests, name='requests'),
      url(r'^admin_tools/', include('admin_tools.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+try:
+    if settings.DEBUG: # defined in manage.py when the first arg is "runserver"
+         urlpatterns += patterns('',
+            (r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
+            (r'^media-admin/(?P<path>.*)$', 'django.views.static.serve',{'document_root': os.path.join(settings.MEDIA_ROOT, '..', settings.ADMIN_MEDIA_PREFIX)}),
+          )
+except NameError:
+    pass
