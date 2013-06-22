8e597101fc6e77b55f539f3a43e83aabf52c91ea
alexa-sandra <alexa.sandra87@gmail.com>
2013-06-22 21:50:42 +0300

    ticket:14 form errors

--- test42cc/media/js/script.js
+++ test42cc/media/js/script.js
@@ -5,11 +5,21 @@ $(function(){
                       $('#progress').show();
                       $('input:submit').attr("disabled","disabled");
                                              },
-                 success : function() {
-                      $('#progress').hide();
+                 success : function(responce) {
+                      $("#progress").hide();
                       $('input:submit').removeAttr("disabled");
-                                          }
-                                       });
-                   return false;
+                      data = $.parseJSON(responce);
+                      alert(responce);
+                      alert(data['errors']);
+                      alert(data['status']);
+                      if (data['status'] == 1){
+                          err = data['errors'];
+                          $.each(err, function(name, error) {
+                              $("#errors_list").append(name + ": " + error);
+                           });
+                                                   }
+                 }
+                 });
+                 return false;
                                               });
                    });
--- test42cc/person/views.py
+++ test42cc/person/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader, Context
+import json
 from models import Person, HttpStoredQuery
 from forms import PersonForm
 
@@ -22,7 +23,7 @@ def edit(request):
     try:
         info = Person.objects.get(pk=1)
     except Person.DoesNotExist:
-        return render(request,'index.html',{'info':None})
+        return render(request, 'index.html', {'info': None})
 
     if request.is_ajax():#submit ajax form
         form = PersonForm(request.POST, request.FILES, instance=info)
@@ -53,16 +54,24 @@ def storedRequests(request):
 
 
 def create_new_account(request):
+    """
+    View for saving new Person entry
+    :param request:
+    :return:
+    """
     if request.is_ajax():
         form = PersonForm(request.POST, request.FILES)
         if form.is_valid():
             if request.FILES:
                 form.cleaned_data['photo'] = request.FILES['photo']
             form.save()
-
-        return render(request, 'edit.html', locals())
-
-
+            return HttpResponse(json.dumps({'status': 0}))
+        else:
+            errors = form.errors
+            print errors
+            #print locals()
+            return HttpResponse(json.dumps({'status': 1, 'errors': errors}))
+        #return render(request, 'edit.html', {'errors': errors})
     else:
         form = PersonForm()
         return render(request, 'edit.html', locals())
